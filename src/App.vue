a5e325abf17521d4d5c7370ec7b954a152015e88
peirong <liupeirongdg@163.com>
2020-11-28 13:24:08 +0800

    init element and echarts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-  html, body, #app  {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
 </style>
