1f849e7382d9db00758ea2c053423e50398b3702
peirong <liupeirongdg@163.com>
2020-12-08 23:41:20 +0800

    salesView 头部开发
    salesView 头部开发

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,15 +1,122 @@
 <template>
-  <div>
-    salesview
+  <div class="salesView">
+    <el-card shadow="hover">
+      <template v-slot:header>
+        <div class="menuWrapper">
+          <el-menu
+            :default-active="activeIndex"
+            mode="horizontal"
+            class="salesViewMenu"
+            @select="onMenuSelect"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menuRight">
+            <el-radio-group v-model="radioSelect" size="small">
+              <el-radio-button label="今日" />
+              <el-radio-button label="本周" />
+              <el-radio-button label="本月" />
+              <el-radio-button label="今年" />
+            </el-radio-group>
+            <el-date-picker
+              v-model="date"
+              type="daterange"
+              range-separator="至"
+              start-placeholde="开始日期"
+              end-placeholde="结束日期"
+              size="small"
+              unlink-panels
+              :picker-options="pickerOptions"
+              class="datePick"
+            />
+          </div>
+        </div>
+      </template>
+      <template>
+        <div>22</div>
+      </template>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SalesView'
+  name: 'SalesView',
+  data() {
+    return {
+      activeIndex: '1',
+      radioSelect: '今日',
+      date: null,
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick(picker) {
+            const start = new Date()
+            const end = new Date()
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一个月',
+          onClick(picker) {
+            const start = new Date()
+            const end = new Date()
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 30)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近三个月',
+          onClick(picker) {
+            const start = new Date()
+            const end = new Date()
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 90)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    onMenuSelect(index) {
+      this.activeIndex = index
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .salesView {
+    margin-top: 20px;
+    .menuWrapper {
+      position: relative;
+      display: flex;
+      .salesViewMenu {
+        width: 100%;
+        padding-left: 20px;
+        .el-menu-item {
+          height: 50px;
+          line-height: 50px;
+          margin: 0 20px;
+        }
+      }
+      .menuRight {
+        position: absolute;
+        right: 20px;
+        top: 0;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        .datePick {
+          margin-left: 20px;
+        }
+      }
+    }
+  }
 
+  ::v-deep .el-card__header {
+    border-bottom: none;
+    padding: 0;
+  }
 </style>
--- src/components/TopView/TodayUsers.vue
+++ src/components/TopView/TodayUsers.vue
@@ -4,7 +4,8 @@
     value="604920"
   >
     <template>
-      <div id="todayUserChart" :style="{ width: '100%', height: '100%' }" />
+      <v-chart :options="getOptions()" />
+      <!-- <div id="todayUserChart" :style="{ width: '100%', height: '100%' }" /> -->
     </template>
     <template v-slot:footer>
       <span>退款率 </span>
@@ -17,32 +18,32 @@
 import commonCardMixin from '../mixins/commonCardMixin'
 export default {
   mixins: [commonCardMixin],
-  mounted() {
-    const chartDom = document.getElementById('todayUserChart')
-    const chart = this.$echarts.init(chartDom)
-    chart.setOption({
-      xAxis: {
-        type: 'category',
-        show: false,
-        data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00']
-      },
-      yAxis: {
-        show: false
-      },
-      series: [{
-        type: 'bar',
-        data: [30, 20, 60, 56, 20, 57, 29],
-        barWidth: '60%'
-      }],
-      color: ['#3398db'],
-      tooltip: {},
-      grid: {
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0
+  methods: {
+    getOptions() {
+      return {
+        xAxis: {
+          type: 'category',
+          show: false,
+          data: ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00']
+        },
+        yAxis: {
+          show: false
+        },
+        series: [{
+          type: 'bar',
+          data: [30, 20, 60, 56, 20, 57, 29],
+          barWidth: '60%'
+        }],
+        color: ['#3398db'],
+        tooltip: {},
+        grid: {
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0
+        }
       }
-    })
+    }
   }
 
 }
